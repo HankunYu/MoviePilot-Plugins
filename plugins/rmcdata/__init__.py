f62b3313b0be490e7730cbb8f3fc486c88a446bf
HankunYu <KmcyD1@gmail.com>
2023-11-09 17:28:34 +0000

    fix

--- plugins/rmcdata/__init__.py
+++ plugins/rmcdata/__init__.py
@@ -18,7 +18,7 @@ class RmCdata(_PluginBase):
     # 主题色
     plugin_color = "#32699D"
     # 插件版本
-    plugin_version = "0.1"
+    plugin_version = "0.2"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
