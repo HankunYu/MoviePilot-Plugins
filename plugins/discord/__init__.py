ef5c31b78787aeffc893ae681331e08a56aff1e3
HankunYu <KmcyD1@gmail.com>
2023-11-10 12:18:26 +0000

    update all to 1.2

--- plugins/discord/__init__.py
+++ plugins/discord/__init__.py
@@ -19,7 +19,7 @@ class Discord(_PluginBase):
     # 主题色
     plugin_color = "#3B5E8E"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
