53b0eed0b052ddbdc737dafc46731ec4f94af3a5
HankunYu <KmcyD1@gmail.com>
2023-11-18 13:47:25 +0000

    add rm_empty function

--- plugins/rmcdata/__init__.py
+++ plugins/rmcdata/__init__.py
@@ -18,7 +18,7 @@ class RmCdata(_PluginBase):
     # 主题色
     plugin_color = "#32699D"
     # 插件版本
-    plugin_version = "1.2"
+    plugin_version = "1.2.1"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
@@ -33,6 +33,7 @@ class RmCdata(_PluginBase):
     # 私有属性
     _enabled = False
     _rm_all = False
+    _rm_empty = False
     _all_path = ""
     _is_running = False
     _threads = []
@@ -41,8 +42,8 @@ class RmCdata(_PluginBase):
             self._enabled = config.get("enabled")
             self._rm_all = config.get("rm_all")
             self._all_path = config.get("all_path")
+            self._rm_empty = config.get("rm_empty")
 
-        # 暂时移除全媒体库修复功能
         if self._rm_all and not self._is_running:
             self._is_runing = True 
             for path in self._all_path.split('\n'):
@@ -106,6 +107,18 @@ class RmCdata(_PluginBase):
                                     {
                                         'component': 'VSwitch',
                                         'props': {
+                                            'model': 'rm_empty',
+                                            'label': '自动删除内容为空的nfo文件',
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                'component': 'VCol',
+                                'content': [
+                                    {
+                                        'component': 'VSwitch',
+                                        'props': {
                                             'model': 'rm_all',
                                             'label': '运行一次全媒体库nfo修复',
                                         }
@@ -136,18 +149,40 @@ class RmCdata(_PluginBase):
                             }
                         ]
                     },
+                    {
+                        'component': 'VRow',
+                        'content': [
+                            {
+                                'component': 'VCol',
+                                'props': {
+                                    'cols': 12,
+                                },
+                                'content': [
+                                    {
+                                        'component': 'VAlert',
+                                        'props': {
+                                            'type': 'info',
+                                            'variant': 'flat',
+                                            'text': '当自动删除内容为空的nfo文件启用时，会在每次nfo文件删除CDATA标签以及全媒体库nfo文件扫描完成后自动删除内容为空的nfo文件'
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
+                    }
                 ]
             }
         ], {
             "enabled": False,
             "rm_all": False,
+            "rm_empty": False,
             "all_path": "",
         }
 
     def get_page(self) -> List[dict]:
         pass
     @staticmethod
-    def replace_cdata_tags(file_path):
+    def replace_cdata_tags(file_path, rm_empty=False):
         logger.info(f'正在处理 {file_path}...')
         with open(file_path, 'r') as file:
             content = file.read()
@@ -156,14 +191,37 @@ class RmCdata(_PluginBase):
         with open(file_path, 'w') as file:
             file.write(content)
             logger.info(f'{file_path} 处理完成')
-            
+            if rm_empty:
+                RmCdata.delete_file_without_plot(file_path)
+    
+    @staticmethod
+    def delete_file_without_plot(file_path):
+        with open(file_path, "r") as file:
+            text = file.read()
+
+            start_tag = "<plot>"
+            end_tag = "</plot>"
+
+            start_index = text.find(start_tag)
+            end_index = text.find(end_tag)
+
+            if start_index != -1 and end_index != -1 and end_index >= start_index + len(start_tag):
+                content = text[start_index + len(start_tag):end_index]
+                if content.strip():
+                    pass
+                else:
+                    logger.info(f'plot标签为空，删除 {file_path}...')
+                    os.remove(file_path)
+            else:
+                logger.info("元数据没找到plot标签，跳过...")
+
     def process_all_nfo_files(self,directory):
         logger.info(f'正在处理 {directory} 下的所有 nfo 文件...')
         for root, dirs, files in os.walk(directory):
             for file in files:
                 if file.endswith('.nfo'):
                     file_path = os.path.join(root, file)
-                    self.replace_cdata_tags(file_path)
+                    self.replace_cdata_tags(file_path, self._rm_empty)
                         
         logger.info(f'{directory} - 处理完成')
         self._threads.pop(0)
@@ -205,15 +263,14 @@ class RmCdata(_PluginBase):
 
 
         raw_data = __to_dict(event.event_data)
-        targets = raw_data.get("transferinfo").get("file_list_new")
+        targets_file = raw_data.get("transferinfo").get("file_list_new")
 
-        for media in targets:
-            logger.info(f'test {media}...')
-            file_name, file_ext = os.path.splitext(media)
+        for media_name in targets_file:
+            file_name, file_ext = os.path.splitext(media_name)
             nfo_file = file_name + ".nfo"
             if os.path.exists(nfo_file):
                 logger.info(f'准备处理 {nfo_file}...')
-                self.replace_cdata_tags(nfo_file)
+                self.replace_cdata_tags(nfo_file, self._rm_empty)
             else:
                 logger.error(f'{nfo_file} 不存在')
 
