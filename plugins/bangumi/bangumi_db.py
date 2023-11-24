cc099a043d8689c9af61e12ca2f63866a6ecf20b
HankunYu <KmcyD1@gmail.com>
2023-11-24 03:48:24 +0000

    test

--- plugins/bangumi/bangumi_db.py
+++ plugins/bangumi/bangumi_db.py
@@ -3,15 +3,15 @@ from app.db import db_query
 from sqlalchemy import or_
 from sqlalchemy.orm import Session
 from app.db import Engine, DbOper
+from app.db.mediaserver_oper import MediaServerOper
 
-class BangumiDbOper(DbOper):
-    def __init__(self, db: Session = None):
-        super().__init__(db)
+class BangumiDbOper():
+    db_oper = MediaServerOper()
+    db = db_oper._db
 
     @db_query
-    def get_media_in_library(self, media_server: list,):
-        media_in_library = self._db.query(MediaServerItem).filter(
-            MediaServerItem.server.in_(media_server),
-            MediaServerItem.item_type.in_(["电影","电视剧"])
+    def get_media_in_library(self, media_server: list):
+        media_in_library = self.db.query(MediaServerItem).filter(
+            MediaServerItem.server.in_(media_server)
         ).all()
         return media_in_library
