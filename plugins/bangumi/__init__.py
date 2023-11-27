17005a961466e5dbd8f9ee59fe3a85ea9a5c402d
HankunYu <KmcyD1@gmail.com>
2023-11-27 21:37:22 +0000

    fix

--- plugins/bangumi/__init__.py
+++ plugins/bangumi/__init__.py
@@ -46,7 +46,7 @@ class Bangumi(_PluginBase):
     # 主题色
     plugin_color = "#5378A4"
     # 插件版本
-    plugin_version = "0.120"
+    plugin_version = "0.121"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
@@ -509,7 +509,6 @@ class Bangumi(_PluginBase):
                                 'props': {
                                     'value': 1,
                                 },
-                                ,
                                 'on': {
                                     'click': () => this.tab = 1
                                 },
