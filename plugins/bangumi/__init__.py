ae8924cdc7974976c3e0832393500c1e1e21545b
HankunYu <KmcyD1@gmail.com>
2023-11-27 21:55:58 +0000

    test

--- plugins/bangumi/__init__.py
+++ plugins/bangumi/__init__.py
@@ -46,7 +46,7 @@ class Bangumi(_PluginBase):
     # 主题色
     plugin_color = "#5378A4"
     # 插件版本
-    plugin_version = "0.123"
+    plugin_version = "0.124"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
@@ -515,7 +515,15 @@ class Bangumi(_PluginBase):
                                         'props': {
                                             'text': '想看',
                                         }
-                                    }
+                                    },
+                                    {
+                                        'component': 'VWindowItem',
+                                        'props': {
+                                            'class': 'grid gap-3 grid-info-card',
+                                            'value': 1,
+                                        },
+                                        'content': contents_wish
+                                    },
                                 ]
                             },
                             {
@@ -529,7 +537,15 @@ class Bangumi(_PluginBase):
                                         'props': {
                                             'text': '看过',
                                         }
-                                    }
+                                    },
+                                    {
+                                        'component': 'VWindowItem',
+                                        'props': {
+                                            'class': 'grid gap-3 grid-info-card',
+                                            'value': 2,
+                                        },
+                                        'content': contents_watched
+                                    },
                                 ]
                             },
                             {
@@ -565,7 +581,7 @@ class Bangumi(_PluginBase):
                     {
                         'component': 'VWindow',
                         'props': {
-                            "model-value": "window",
+                            "model-value": "tab",
                             "show-arrows": True,
                         },
                         'content': [
