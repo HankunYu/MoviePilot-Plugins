64a3b0a07330e0713d43b58f699a90ad78e1579f
HankunYu <KmcyD1@gmail.com>
2024-01-23 01:20:19 +0000

    fix path issue

--- plugins/discord/discord_bot.py
+++ plugins/discord/discord_bot.py
@@ -13,9 +13,7 @@ client = commands.Bot(command_prefix='$', intents=intents)
 
 # Load cogs
 async def load_extensions():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            await client.load_extension(f"cogs.{filename[:-3]}")
+    await client.load_extension(f"cogs.moviepilot_cog")
 
 async def run_bot():
     async with client:
