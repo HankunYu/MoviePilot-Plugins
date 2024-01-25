b48c1128c1e11751207857ac11426bfe06cba98f
HankunYu <KmcyD1@gmail.com>
2024-01-25 16:19:39 +0000

    再次测试

--- plugins/discord/discord_bot.py
+++ plugins/discord/discord_bot.py
@@ -1,6 +1,7 @@
 import discord, discord.webhook, asyncio, os
 from discord.ext import commands
 import plugins.discord.tokenes as tokenes
+import plugins.discord.cogs.moviepilot_cog as moviepilot_cog
 from app.log import logger
 """
 bot 本体.
@@ -16,8 +17,11 @@ async def load_extensions():
     directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cogs")
     for filename in os.listdir(directory):
         if filename.endswith(".py"):
-            logger.info(f"Loading {filename[:-3]}")  
-            await client.load_extension(f"{filename[:-3]}")
+            logger.info(f"Loading {filename[:-3]}")
+            try:
+                await client.load_extension(f"{filename[:-3]}")
+            except client.load_extension.ExtensionNotFound as e:
+                logger.error(f"Failed to load {filename[:-3]}: {e}")
 
 async def run_bot():
     logger.info("Discord bot 启动中...")
