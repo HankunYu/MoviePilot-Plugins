ccf3216c8b0070f7e55cd91eadb2c1c084166a17
HankunYu <KmcyD1@gmail.com>
2024-01-25 17:34:19 +0000

    Fix openai 更新导致的失败

--- plugins/discord/gpt.py
+++ plugins/discord/gpt.py
@@ -1,6 +1,10 @@
-import openai
+from openai import OpenAI
 import plugins.discord.tokenes as tokenes
-openai.api_key = tokenes.gpt_token
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=tokenes.gpt_token,
+)
 chat_history = [
             {"role": "system", "content": "你是一个智能助手,请用中文回复我"}
         ]
@@ -17,12 +21,13 @@ def generate_reply(message):
     # chat_history 添加用户输入
     chat_history.append({"role": "user", "content": message})
 
-    chat = openai.ChatCompletion.create(
+    chat = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages = chat_history,
         )
-    print(chat.choices[0].message.get('content'))
+    # print(chat)
+    print(chat.choices[0].message.content)
     # chat_history 添加助手回复
-    chat_history.append({"role": "assistant", "content": chat.choices[0].message.get('content')})
+    chat_history.append({"role": "assistant", "content": chat.choices[0].message.content})
 
-    return chat.choices[0].message.get('content')
+    return chat.choices[0].message.content
