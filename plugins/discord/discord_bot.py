469fdf3ad294bacb039e7600d9dd2b8652bcda5e
HankunYu <KmcyD1@gmail.com>
2024-01-25 16:34:32 +0000

    test

--- plugins/discord/discord_bot.py
+++ plugins/discord/discord_bot.py
@@ -14,14 +14,16 @@ client = commands.Bot(command_prefix='$', intents=intents)
 
 # Load cogs
 async def load_extensions():
-    directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cogs")
-    for filename in os.listdir(directory):
-        if filename.endswith(".py"):
-            logger.info(f"Loading {filename[:-3]}")
-            try:
-                await client.load_extension(f"{directory}/{filename[:-3]}")
-            except Exception as e:
-                logger.error(f"Failed to load {filename[:-3]}: {e}")
+    
+    await client.load_extension(f"plugins.discord.cogs.moviepilot_cog")
+    # directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cogs")
+    # for filename in os.listdir(directory):
+    #     if filename.endswith(".py"):
+    #         logger.info(f"Loading {filename[:-3]}")
+    #         try:
+    #             await client.load_extension(f"{directory}/{filename[:-3]}")
+    #         except Exception as e:
+    #             logger.error(f"Failed to load {filename[:-3]}: {e}")
 
 async def run_bot():
     logger.info("Discord bot 启动中...")
