9db0d1edb098894d1995c5119d9b89b885fac03a
HankunYu <KmcyD1@gmail.com>
2024-01-26 17:12:33 +0000

    fix

--- plugins/discord/__init__.py
+++ plugins/discord/__init__.py
@@ -24,7 +24,7 @@ class Discord(_PluginBase):
     # 主题色
     plugin_color = "#3B5E8E"
     # 插件版本
-    plugin_version = "1.3.45"
+    plugin_version = "1.3.46"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
@@ -64,16 +64,16 @@ class Discord(_PluginBase):
             self._site_url = config.get("site_url")
             self._bot_token = config.get("bot_token")
             self._select_types = config.get("select_types")
+            
+            tokenes.bot_token = self._bot_token
+            tokenes.gpt_token = self._gpt_token
 
             if(self._site_url and not self._site_url.startswith("http")):
                 self._site_url = "http://" + self._site_url
             # 启动discord bot
             if(self._enabled and self._bot_token):
-                tokenes.bot_token = self._bot_token
-                tokenes.gpt_token = self._gpt_token
-                if not self.bot_thread:
-                    self.bot_thread = threading.Thread(target=self.bot_start)
-                    self.bot_thread.start()
+                self.bot_thread = threading.Thread(target=self.bot_start)
+                self.bot_thread.start()
             else:
                 if(self.bot_thread and self._enabled == False):
                     asyncio.run(discord_bot.stop())
