5b795c6d41dfb2ae357784fe99bf21a7a8bc23e4
HankunYu <KmcyD1@gmail.com>
2024-01-26 13:02:44 +0000

    测试command

--- plugins/discord/cogs/moviepilot_cog.py
+++ plugins/discord/cogs/moviepilot_cog.py
@@ -5,11 +5,9 @@ from app.log import logger
 from app.chain.download import DownloadChain
 from app.chain.search import SearchChain
 from app.chain.subscribe import SubscribeChain
+from app.core.context import MediaInfo, TorrentInfo, Context
 from app.core.metainfo import MetaInfo
-try:
-    import plugins.discord.gpt as gpt
-except:
-    logger.error("GPT模块载入失败")
+import plugins.discord.gpt as gpt
 
 class MPCog(commands.Cog):
     on_conversion = False
@@ -67,15 +65,72 @@ class MPCog(commands.Cog):
     @app_commands.command(description="自动搜索并下载电影")
     async def download(self, interaction: discord.Interaction, title: str):
         await interaction.response.send_message("正在下载电影 " + title)
+    
+    @app_commands.command(description="自动搜索并下载电影")
+    async def subscribe(self, interaction: discord.Interaction, title: str):
+        await interaction.response.send_message("正在订阅 " + title)
         
-    def download_chain(self, title: str):
-        pass
-    
-    def search_chain(self, title: str):
-        pass
-    
-    def subscribe_chain(self, title: str):
-        pass
+
+    @app_commands.command(description="搜索电影")
+    async def search(self, interaction: discord.Interaction, title: str):
+        await interaction.response.send_message("正在搜索电影 " + title)
+        mediaInfo = MediaInfo(title = title)
+        contexts = self.searchchain.process(mediainfo = mediaInfo)
+        if len(contexts) == 0:
+            await interaction.response.send_message("没有找到资源 " + title)
+        else:
+            for context in contexts:
+                torrent = context.torrent
+                embed = discord.Embed(title=torrent.title, 
+                                      description=torrent.description,
+                                      url=torrent.page_url)
+                fields = []
+                site_name = {
+                    "name": "站点",
+                    "value": torrent.site_name
+                }
+                torrent_size = {
+                    "name": "种子大小",
+                    "value": torrent.size
+                }
+                seeders = {
+                    "name": "做种数",
+                    "value": torrent.seeders
+                }
+                peers = {
+                    "name": "下载数",
+                    "value": torrent.peers
+                }
+                fields.append(site_name)
+                fields.append(torrent_size)
+                fields.append(seeders)
+                fields.append(peers)
+
+                for field in fields:
+                    embed.add_field(name=field[0], value=field[1], inline=True)
+                
+                view = DownloadView(context, self.downloadchain)
+                await interaction.response.send_message(embed=embed, view=view)
+
+class DownloadView(discord.ui.View):
+    context = None
+    def __init__(self, context: Context, downloadchain: DownloadChain):
+        super().__init__(timeout=180)
+        self.context = context
+
+    @discord.ui.button(label="下载", style = discord.ButtonStyle.blurple)
+    async def download(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.downloadchain.dowload_single(self.context)
+
+class SubscribeView(discord.ui.View):
+    context = None
+    def __init__(self, context: Context, subscribechain: SubscribeChain):
+        super().__init__(timeout=180)
+        self.context = context
+
+    @discord.ui.button(label="订阅", style = discord.ButtonStyle.blurple)
+    async def subscribe(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.subscribechain.add(self.context, username = "Discord Bot")
 
 async def setup(bot : commands.Bot):
     await bot.add_cog(MPCog(bot))
