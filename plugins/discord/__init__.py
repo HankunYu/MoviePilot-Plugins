90d505693cff49111e2c3d5f1f8d3b17032fb103
HankunYu <KmcyD1@gmail.com>
2024-01-28 13:15:31 +0000

    test

--- plugins/discord/__init__.py
+++ plugins/discord/__init__.py
@@ -24,7 +24,7 @@ class Discord(_PluginBase):
     # 主题色
     plugin_color = "#3B5E8E"
     # 插件版本
-    plugin_version = "1.3.73"
+    plugin_version = "1.3.74"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
@@ -83,8 +83,7 @@ class Discord(_PluginBase):
             else:
                 # 如果插件被禁用，停止discord bot
                 if(self.bot_thread and self._enabled == False):
-                    logger.info("Discord bot 停止中...")
-                    discord_bot.client.loop.stop()
+                    asyncio.run_coroutine_threadsafe(discord_bot.stop(), self.loop)
                     self.loop.stop()
                     self.bot_thread = None
                     tokenes.is_bot_running = False                
@@ -559,8 +558,7 @@ class Discord(_PluginBase):
         退出插件
         """
         if(self.bot_thread):
-            logger.info("Discord bot 停止中...")
-            discord_bot.client.loop.stop()
+            asyncio.run_coroutine_threadsafe(discord_bot.stop(), self.loop)
             self.loop.stop()
             self.bot_thread = None
             tokenes.is_bot_running = False
