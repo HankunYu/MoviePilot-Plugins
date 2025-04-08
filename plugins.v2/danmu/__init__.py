ae3976694ec316dee680b5ae09f737e58f72214e
HankunYu <KmcyD1@gmail.com>
2025-04-08 19:39:40 +0100

    补充遗漏文件

--- plugins.v2/danmu/__init__.py
+++ plugins.v2/danmu/__init__.py
@@ -26,7 +26,7 @@ class Danmu(_PluginBase):
     # 主题色
     plugin_color = "#3B5E8E"
     # 插件版本
-    plugin_version = "1.1.11"
+    plugin_version = "1.1.12"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
