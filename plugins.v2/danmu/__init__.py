8596259e2649bf9e79b5220a1a3e86c3285cc045
HankunYu <KmcyD1@gmail.com>
2025-04-22 22:37:39 +0100

    update danmu

--- plugins.v2/danmu/__init__.py
+++ plugins.v2/danmu/__init__.py
@@ -30,7 +30,7 @@ class Danmu(_PluginBase):
     # 主题色
     plugin_color = "#3B5E8E"
     # 插件版本
-    plugin_version = "1.1.15.1"
+    plugin_version = "1.2.0"
     # 插件作者
     plugin_author = "hankun"
     # 作者主页
